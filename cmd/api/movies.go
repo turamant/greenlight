6e46adae1d600a8ca82dfd6abc6855e0ac0d7dd4
askvart <tur1amant@gmail.com>
2023-06-29 16:03:33 +0300

    helpers

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,8 +4,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +11,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 	return
